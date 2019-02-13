e95876bb0c95760b14bcd3c9f3f41978f5420490
Bernardo Loureiro <bernardo.lou@gmail.com>
2019-02-13 01:38:20 -0200

    Checking and downloading correct stremio server files

--- setup.sh
+++ setup.sh
@@ -1,24 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+
+TIMENOW=$(date +"%Y%m%d%H%M%S")
 
 get_latest_release_v3() {
-    # curl --silent -H "Accept: application/vnd.github+json" "https://api.github.com/repos/$1/tags" |
-    #     jq -r '.[].name' |
-    #     sed -e 's/v//g' |
-    #     sort --version-sort |
-    #     head -n 1
-    echo '4.4.25'
+    /usr/bin/curl --silent -H "Accept: application/vnd.github+json" "https://api.github.com/repos/$1/tags" |
+        jq -r '.[].name' |
+        sed -e 's/v//g' |
+        sort --version-sort --reverse |
+        head -n 1
+    # echo -e '4.4.25'
 }
 
 get_latest_stremio_release() {
-    get_latest_release_v3 "Stremio/stremio-shell"
+    get_latest_release_v3 'Stremio/stremio-shell'
+}
+
+backup_folder() {
+    /bin/mv $1 $2
+}
+
+download_file() {
+    /usr/bin/curl -i --silent -o $2 $1
 }
 
 get_latest_stremio_server_js() {
+    echo -e 'checking latest stremio version...\n'
+
     LATEST=$(get_latest_stremio_release)
-    BASE_URL="https://dl.strem.io/four/${LATEST}"
-    echo 'latest stremio version is '${LATEST}'. downloading from '${BASE_URL}'...'
-    curl --silent -i -o stremio/server.js ${BASE_URL}/server.js
-    curl --silent -i -o stremio/stremio.asar ${BASE_URL}/stremio.asar
+
+    echo -e 'latest stremio version is '${LATEST}'\n'
+
+    PATH=${PWD}/stremio/v${LATEST}
+
+    echo -e 'path to download is '${PATH}', checking...\n'
+
+    if [ -d ${PATH} ]; then
+        echo -e 'already exist '${PATH}', backuping...\n';
+
+        backup_folder ${PATH} ${PATH}_bkp_${TIMENOW}
+    else
+        echo -e 'there is not this path yet, will be created\n'
+    fi
+
+    /bin/mkdir -p ${PATH}
+
+    BASE_URL=https://dl.strem.io/four/v${LATEST}
+
+    echo -e 'downloading from '${BASE_URL}'...\n'
+
+    download_file ${BASE_URL}/server.js ${PATH}/server.js
+    download_file ${BASE_URL}/stremio.asar ${PATH}/stremio.asar
+
+    echo -e 'finish download stremio server\n'
 }
 
 get_latest_stremio_server_js

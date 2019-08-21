b52a5cca93bfa1e060e6e93fdf73a454244c2bfe
Bernardo Loureiro <305923+bernardolm@users.noreply.github.com>
2019-08-20 23:15:01 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,26 +2,25 @@
 
 source functions.sh
 
-
 get_latest_stremio_server_js
 
+sudo apt purge idle3 ^geany --yes
+sudo apt --purge autoremove --yes
 
-# sudo apt purge idle3 ^geany --yes
-# sudo apt --purge autoremove --yes
+sudo apt-get install debian-keyring debian-archive-keyring debian-multimedia-keyring deb-multimedia-keyring zsh matchbox-keyboard nano realvnc-vnc-viewer --yes
 
-# sudo apt-get install debian-keyring debian-archive-keyring debian-multimedia-keyring deb-multimedia-keyring zsh matchbox-keyboard nano realvnc-vnc-viewer --yes
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm ~/.bash_history
+cp -f .zshrc ~/.zshrc
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# rm ~/.bash_history
-# cp -f .zshrc ~/.zshrc
+sudo wget -O - https://ftp-master.debian.org/keys/archive-key-8.asc | sudo apt-key add -
+sudo wget -O - https://ftp-master.debian.org/keys/archive-key-8-security.asc | sudo apt-key add -
+sudo wget -O - https://ftp-master.debian.org/keys/archive-key-9.asc | sudo apt-key add -
+sudo wget -O - https://ftp-master.debian.org/keys/archive-key-9-security.asc | sudo apt-key add -
 
-# sudo wget -O - https://ftp-master.debian.org/keys/archive-key-8.asc | sudo apt-key add -
-# sudo wget -O - https://ftp-master.debian.org/keys/archive-key-8-security.asc | sudo apt-key add -
-# sudo wget -O - https://ftp-master.debian.org/keys/archive-key-9.asc | sudo apt-key add -
-# sudo wget -O - https://ftp-master.debian.org/keys/archive-key-9-security.asc | sudo apt-key add -
+sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.old
+sudo cp -f sources.list /etc/apt
+sudo cp -f sources.list.d/*.* /etc/apt/sources.list.d
 
-# sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.old
-# sudo cp -f sources.list /etc/apt
-# sudo cp -f sources.list.d/*.* /etc/apt/sources.list.d
-
-# sudo apt update
+sudo apt update
+sudo apt upgrade --yes

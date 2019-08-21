b9f3331fc427965cd0bbefa53821cdd0aaa6cf6a
Bernardo Loureiro <bernardo.lou@gmail.com>
2019-08-21 04:07:35 -0300

    Fixing last stremio version function

--- stremio/stremio.sh
+++ stremio/stremio.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 PWD=`pwd`
-source ${PWD}/../functions.sh
 
-run_server() {
+function get_latest_release_v3() {
+    /usr/bin/curl --silent -H "Accept: application/vnd.github+json" "https://api.github.com/repos/$1/tags" |
+        /usr/bin/jq -r '.[].name' |
+        /bin/grep -v "autoupdate" |
+        /bin/sed -e 's/v//g' |
+        /usr/bin/sort --version-sort --reverse |
+        /usr/bin/head -n 1
+}
+
+function get_latest_stremio_release() {
+    get_latest_release_v3 'Stremio/stremio-shell'
+}
+
+function run_server() {
     LATEST=$(get_latest_stremio_release)
     cd ${PWD}/v${LATEST}
     echo 'running from '`pwd`

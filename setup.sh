4b72f31a57b80ff5500b9878bd700d46fb89243c
Bernardo Loureiro <bernardo.lou@gmail.com>
2019-08-21 02:32:52 -0300

    Updating some commands

--- setup.sh
+++ setup.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+reset
 
+echo -e '\nloading custom functions.sh\n'
 source functions.sh
 
-get_latest_stremio_server_js
-
+echo -e '\nremoving not used packages\n'
 sudo apt purge idle3 ^geany --yes
 sudo apt --purge autoremove --yes
 
-sudo apt-get install debian-keyring raspbian-archive-keyring debian-multimedia-keyring deb-multimedia-keyring zsh matchbox-keyboard nano realvnc-vnc-viewer --yes
+echo -e '\nupdating and adding needed packages\n'
+sudo mv /etc/apt/sources.list.d/raspi.list ~/raspi.list.old
+sudo cp -f sources.list /etc/apt
+sudo apt update
+sudo apt install --yes jq debian-keyring raspbian-archive-keyring zsh matchbox-keyboard curl
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--rm ~/.bash_history
+echo -e '\ninstalling stremio\n'
+get_latest_stremio_server_js
+
+echo -e '\ninstalling oh-my-zsh\n'
+/bin/sh -c "$(/usr/bin/curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv ~/.bash_history ~/.bash_history.old
 cp -f .zshrc ~/.zshrc
 
+echo -e '\nadding custom apt repositories\n'
+sudo cp -f sources.list.d/*.* /etc/apt/sources.list.d
+sudo apt update
+
+echo -e '\nadding custom apt keys\n'
 sudo wget -O - https://ftp-master.debian.org/keys/archive-key-8.asc | sudo apt-key add -
 sudo wget -O - https://ftp-master.debian.org/keys/archive-key-8-security.asc | sudo apt-key add -
 sudo wget -O - https://ftp-master.debian.org/keys/archive-key-9.asc | sudo apt-key add -
 sudo wget -O - https://ftp-master.debian.org/keys/archive-key-9-security.asc | sudo apt-key add -
 
-sudo mv /etc/apt/sources.list.d/raspi.list ~/raspi.list.old
-sudo cp -f sources.list /etc/apt
-sudo cp -f sources.list.d/*.* /etc/apt/sources.list.d
+echo -e '\nadding custom keyring packages\n'
+wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb -O /tmp/deb-multimedia-keyring_2016.8.1_all.deb
+sudo dpkg -i /tmp/deb-multimedia-keyring_2016.8.1_all.deb
 
-sudo apt update
+echo -e '\nupgrading system\n'
 sudo apt upgrade --yes
